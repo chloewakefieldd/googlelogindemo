ebf622f887e94d5171573a3760f3e5ae1d214ec6
Chloe Wakefield <chloe.wakefield@bbc.co.uk>
2020-08-27 20:16:59 +0100

    Add basic Google auth

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import Login from '../components/Login';
+import Logout from '../components/Logout';
+
 const App: React.FunctionComponent = () => {
-  return <div>Hi</div>;
+  return (
+    <>
+      <div>Before login button</div>
+      <Login />
+      <div>After login button and before logout button</div>
+      <Logout />
+      <div>After logout button</div>
+    </>
+  );
 };
 
 export default App;
